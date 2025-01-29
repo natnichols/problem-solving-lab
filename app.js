1320480b9a01db1c145931fff8148f6ec1a6a9b2
NatNichols <natalie.e.nichols@gmail.com>
2025-01-29 13:56:01 -0500

    add exercise names as notes

---
+++ app.js
@@ -0,0 +1,45 @@
+// 1 - printGreeting
+
+
+// 2- reverseWordOrder
+
+
+// 3 - calculate
+
+
+// 4 - printConsecutives
+
+
+// 5 - letterReverse
+
+
+// 6 - calculateCube
+
+
+// 7 - isAVowel
+
+
+// 8 - getTwoLengths
+
+
+// 9 - getMultipleLengths
+
+
+// 10 - maxOfThree
+
+
+// 11 - printLongestWord
+
+
+// 12 - transmogrify
+
+
+// 13 - Project Euler Problem 2
+
+
+// 14 - A Needle in the Haystack
+
+
+// 15 - Sum the Positive
+
+
